9fd974b2b901a398adaea5ba7f9a3bf7ae6590ad
daniellowtw <daniellowtw@gmail.com>
2014-02-17 17:35:21 +0000

    Fixed MapView

--- client/app/view/MapView.js
+++ client/app/view/MapView.js
@@ -5,7 +5,7 @@ Ext.define('moneyworld.view.MapView', {
 	],
 	config: {
 		title: "MapView",
-		html: '<iframe style="position: absolute; width: 100%; height: 100%;" src="app/viz/map_view/index.html"/>'
+		html: '<iframe style="position: absolute; width: 100%; height: 100%;" src="app/viz/map_view/index.html"/>',
 		title: null
 	}
 });
