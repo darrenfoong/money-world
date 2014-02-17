c6ae34ed6334aadb5571764e01bc4d5ec8197ace
Darren Foong <darrenfoong@gmail.com>
2014-02-17 17:06:10 +0000

    Added Functions and updated Unemp views which load data directly from the server.

--- client/app/controller/DetailedViewUnemp.js
+++ client/app/controller/DetailedViewUnemp.js
@@ -5,6 +5,48 @@ Ext.define('moneyworld.controller.DetailedViewUnemp', {
 		refs: {
 			mainView: 'main',
 			detailedViewUnemp: 'detailedview_unemp',
+			detailedViewUnempChart: 'detailedview_unemp chart'
+		},
+		control: {
+			'detailedViewUnemp': {
+				initialize: 'renderView'
+			}
+		}
+	},
+
+	renderView: function() {
+		var settingsStore = Ext.getStore('Settings');
+		var dataSetsStore = Ext.getStore('DataSets');
+		var dataPointsStore;
+
+		var currentCountry;
+		var currentRegion;
+
+		var settingsRecords = [];
+		var dataPointsRecords = [];
+
+		settingsStore.load({ callback: loadDataSetsStore, scope: this });
+
+		function loadDataSetsStore(records, operations, success) {
+			settingsRecords = records;
+			currentCountry = records[0].get('countryCode');
+			currentRegion = records[0].get('region');
+			dataSetsStore.load({ callback: loadDataPointsStore, scope: this });
+		}
+
+		function loadDataPointsStore(records, operation, success) {
+			dataSetsRecords = records;
+			dataPointsStore = moneyworld.utils.Functions.getServerStore(
+				this.getDetailedViewUnemp().getDataSet(),
+				"all",
+				currentCountry);
+			dataPointsStore.load({ callback: setData, scope: this });
+		}
+
+		function setData(records, operation, success) {
+			// Visualisation code starts here
+			this.getDetailedViewUnempChart().setStore(dataPointsStore);
+			// Visualisation code ends here
 		}
 	}
 });
--- client/app/controller/SummaryViewUnemp.js
+++ client/app/controller/SummaryViewUnemp.js
@@ -16,41 +16,40 @@ Ext.define('moneyworld.controller.SummaryViewUnemp', {
 	renderView: function() {
 		var settingsStore = Ext.getStore('Settings');
 		var dataSetsStore = Ext.getStore('DataSets');
-		var dataPointsStore = Ext.create('Ext.data.Store', {
-			model: 'moneyworld.model.DataPoint',
-			proxy: {
-				type: 'ajax',
-				url: 'data/datapoints.json',
-				reader: {
-					type: 'json',
-					rootProperty: 'datapoints'
-				}
-			}
-		});
+		var dataPointsStore;
 
-		dataSetsRecords = [];
-		dataPointsRecords = [];
-		dataSetsStore.load({ callback: loadDataPointsStore, scope: this });
+		var currentCountry;
+		var currentRegion;
+
+		var settingsRecords = [];
+		var dataPointsRecords = [];
+
+		settingsStore.load({ callback: loadDataSetsStore, scope: this });
+
+		function loadDataSetsStore(records, operations, success) {
+			settingsRecords = records;
+			currentCountry = records[0].get('countryCode');
+			currentRegion = records[0].get('region');
+			dataSetsStore.load({ callback: loadDataPointsStore, scope: this });
+		}
 
 		function loadDataPointsStore(records, operation, success) {
 			dataSetsRecords = records;
-			dataPointsStore.load({ callback: loadSettings, scope: this });
-		}
-
-		function loadSettings(records, operation, success) {
-			dataPointsRecords = records;
-			settingsStore.load({ callback: setData, scope: this });
+			dataPointsStore = moneyworld.utils.Functions.getServerStore(
+				this.getSummaryViewUnemp().getDataSet(),
+				"all",
+				currentCountry);
+			dataPointsStore.load({ callback: setData, scope: this });
 		}
 
 		function setData(records, operation, success) {
-			var currentCountry = records[0].get('countryCode');
-			var currentRegion = records[0].get('region');
 			// Visualisation code starts here
+			dataPointsStore.sort([{ property: 'year', direction: 'ASC'}]);
 			dataPointsStore.filter([
 				Ext.create('Ext.util.Filter', { property: 'countryCode', value: currentCountry }),
 				Ext.create('Ext.util.Filter', { property: 'dataSetCode', value: this.getSummaryViewUnemp().getDataSet() })
 			]);
-			var currentUnemp = dataPointsStore.first().get('value');
+			var currentUnemp = dataPointsStore.last().get('value');
 			this.getSummaryViewUnemp().setHtml(currentUnemp);
 			// Visualisation code ends here
 		}
