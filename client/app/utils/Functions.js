7b48b87a8c1b46a39ab3114b1457e4bd7169dbc0
Darren Foong <darrenfoong@gmail.com>
2014-02-18 13:36:30 +0000

    Added floatToRatio to convert floating point values to approximate ratios for visualisations.

--- client/app/utils/Functions.js
+++ client/app/utils/Functions.js
@@ -38,5 +38,22 @@ Ext.define('moneyworld.utils.Functions', {
 		output += pvalue + " " + units[counter] + " " + suffix;
 
 		return output;
+	},
+
+	floatToRatio: function(value) {
+		var denominator = 20;
+		var numerator = parseInt(value * denominator);
+		
+		function gcd(a, b) {
+			if ( b == 0 ) {
+				return a;
+			} else {
+				return gcd(b, a % b);
+			}
+		}
+
+		var factor = gcd(numerator, denominator);
+		console.log(value + " becomes " + numerator/factor + "/" + denominator/factor);
+		return { numerator: numerator/factor, denominator: denominator/factor }
 	}
 });
