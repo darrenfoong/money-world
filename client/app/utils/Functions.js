524a360ead658715ee0b2ad4c6316f29509a88cd
Darren Foong <darrenfoong@gmail.com>
2014-02-18 16:29:44 +0000

    Updated floatToGrid.

--- client/app/utils/Functions.js
+++ client/app/utils/Functions.js
@@ -43,8 +43,8 @@ Ext.define('moneyworld.utils.Functions', {
 	floatToRatio: function(value) {
 		// Input: a floating point number between 0 and 1
 		// Output: an object with numerator and denominator fields
-		var denominator = 20;
-		var numerator = parseInt(value * denominator);
+		var denominator = 100;
+		var numerator = Math.ceil(value * denominator);
 		
 		function gcd(a, b) {
 			if ( b == 0 ) {
@@ -62,17 +62,17 @@ Ext.define('moneyworld.utils.Functions', {
 		// Input: a floating point number between 0 and 1
 		// Output: an object with numerator, denominator, width of grid, and height of grid fields
 		// Algorithm tries to create a grid that is as square as possible.
+		// TODO Improve to include factorisation of arbitrary denominator; pair of factors with the smallest difference is the solution
 		var ratio = moneyworld.utils.Functions.floatToRatio(value);
 		var numerator = ratio.numerator;
 		var denominator = ratio.denominator;
 
 		var i = 0;
-		var primes = [2,3,5,7,9,13,17,19];
+		var primes = [2,3,5,7,9];
 
 		var width = 1;
-		var limit = Math.floor(Math.sqrt(denominator));
 
-		while ( width < limit ) {
+		while ( width < denominator ) {
 			if ( denominator % primes[i] == 0 ) {
 				denominator /= primes[i];
 				width *= primes[i];
