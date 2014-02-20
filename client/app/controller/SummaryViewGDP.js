23ed675bb667913ee1d76a5d7c80d6bd781fe3c0
Jovan Powar <jovan@powarup.com>
2014-02-20 16:01:12 +0000

    Added circles and text to SummaryViewGDP

--- client/app/controller/SummaryViewGDP.js
+++ client/app/controller/SummaryViewGDP.js
@@ -1,6 +1,6 @@
 Ext.define('moneyworld.controller.SummaryViewGDP', {
 	extend: 'moneyworld.controller.SummaryView',
-	
+
 	config: {
 		refs: {
 			mainView: 'main',
@@ -8,14 +8,83 @@ Ext.define('moneyworld.controller.SummaryViewGDP', {
 		},
 		control: {
 			'summaryViewGDP': {
+				initialize: 'renderView'
 			}
 		}
 	},
-
+	
 	renderView: function() {
+		console.log("initializing SummaryViewGDP");
 		
-	},
-	initConfig: function() {
+		/*
+		var settingsStore = Ext.getStore('Settings');
+		var dataSetsStore = Ext.getStore('DataSets');
+		var dataPointsStore;
+
+		var currentCountry;
+		var currentRegion;
+
+		var settingsRecords = [];
+		var dataPointsRecords = [];
+
+		settingsStore.load({ callback: loadDataSetsStore, scope: this });
+
+		function loadDataSetsStore(records, operations, success) {
+			settingsRecords = records;
+			currentCountry = records[0].get('countryCode');
+			currentRegion = records[0].get('region');
+			dataSetsStore.load({ callback: loadDataPointsStore, scope: this });
+		}
+
+		function loadDataPointsStore(records, operation, success) {
+			dataSetsRecords = records;
+			dataPointsStore = moneyworld.utils.Functions.getServerStore(
+				this.getSummaryViewInflation().getDataSet(),
+				"all",
+				currentCountry);
+			dataPointsStore.load({ callback: setData, scope: this });
+		}
+
+		function setData(records, operation, success) {
+			// Visualisation code starts here
+			dataPointsStore.sort([{ property: 'year', direction: 'ASC'}]);
+			dataPointsStore.filter([
+				Ext.create('Ext.util.Filter', { property: 'countryCode', value: currentCountry }),
+				Ext.create('Ext.util.Filter', { property: 'dataSetCode', value: this.getSummaryViewGDP().getDataSet() })
+			]);
+			var currentGDP = dataPointsStore.last().get('value');
+		*/
+		
+		var centrex = 100, centrey = 100, circler = 50;
+		this.getSummaryViewGDP().add({
+			xtype: 'panel',
+			layout: 'card',
+			height: '90%',
+			width: '100%',
+			style: {
+				background: 'red'
+			},
+			initialize: function() {
+				var drawComponent1 = Ext.create('Ext.draw.Component', {});
+				drawComponent1.getSurface('main').add({
+					type: 'circle',
+					fill: '#79BB3F',
+					radius: circler,
+					x: this.width/2,
+					y: centrey
+				});
+				this.add(drawComponent1);
+			}
+		});
+		
+		this.getSummaryViewGDP().add({
+			xtype: 'panel',
+			layout: 'card',
+			height: '10%',
+			width: '100%',
+			style: 'text-align: center',
+			html: "<div>GDP is n</div>"
+		});
 		
 	}
 });
--- client/app/view/SummaryViewGDP.js
+++ client/app/view/SummaryViewGDP.js
@@ -1,8 +1,9 @@
 Ext.define('moneyworld.view.SummaryViewGDP', {
-	extend: 'moneyworld.view.SummaryViewCircle',
+	extend: 'moneyworld.view.SummaryView',
 	xtype: 'summaryview_gdp',
 	requires: [
-		'Ext.draw.*'
+		'Ext.draw.*',
+		'moneyworld.view.SampleDraw'
 	],
 	config: {
 		dataSet: null, //find GDP stuff
@@ -13,33 +14,40 @@ Ext.define('moneyworld.view.SummaryViewGDP', {
 
 		height: '95%',
 
-		//html: '<text>Hello World!</text>',
-		/*
-		items: [{
-        	xtype: 'draw',
-            sprites: [
-        	{
-            type: 'circle',
-            fillStyle: '#ff8',
-            strokeStyle: 'black',
-            lineWidth: 3,
-            radius: 100,
-            x: 100,
-            y: 100
-        	}]
-    	}]*/
+		items:[
+			/*{
+			xtype: 'panel',
+			layout: 'card',
+			height: '90%',
+			width: '100%',
+			style: {
+				background: 'red'
+			},
+			initialize: function() {
+				var drawComponent1 = Ext.create('Ext.draw.Component', {});
+				drawComponent1.getSurface('main').add({
+					type: 'circle',
+					fill: '#79BB3F',
+					radius: 100,
+					x: 100,
+					y: 100
+				});
+					
+				drawComponent1.getSurface('main').add({
+					type: "path",
+					path: "M200 200 L270 205",
+					fill: "blue",
+					lineWidth: 7,
+					fillStyle: 'rgba(155, 0, 255, .2)',
+					strokeStyle: 'purple',
+					id: 'hoursHand',
+					rotationRads: 1,
+					rotationCenterX: 200,
+					rotationCenterY: 200
+				});
+				this.add(drawComponent1);
+			}
+		}*/
+		]
 	},
-	initialize: function() {
-		alert("working");
-		new Ext.draw.Component({
-  			fullscreen: true,
-  			items: [{
-    		type: 'circle',
-    		cx: 100,
-    		cy: 100,
-    		r: 25,
-    		fillStyle: 'blue'
-  		}]
-		});
-	}
 });
