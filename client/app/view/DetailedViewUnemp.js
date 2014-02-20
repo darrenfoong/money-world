b0f35c47c9369eb46965cacdd62b63fbdac11ab4
Darren Foong <darrenfoong@gmail.com>
2014-02-20 14:36:38 +0000

    Added dependencies to DetailedViewUnemp to remove warnings

--- client/app/view/DetailedViewUnemp.js
+++ client/app/view/DetailedViewUnemp.js
@@ -2,6 +2,8 @@ Ext.define('moneyworld.view.DetailedViewUnemp', {
 	extend: 'moneyworld.view.DetailedView',
 	xtype: 'detailedview_unemp',
 	requires: [
+		'Ext.chart.series.Line',
+		'Ext.chart.interactions.ItemInfo'
 	],
 	config: {
 		items: [
