c48a76fd141e102131e4ecf2fc454d98a4a6c205
Daniel Low <daniellowtw@gmail.com>
2014-02-25 23:18:30 +0000

    reverted back to transparent cake image, added dynamic life expectancy value

--- client/app/controller/SummaryViewLifeExpectancy.js
+++ client/app/controller/SummaryViewLifeExpectancy.js
@@ -8,73 +8,110 @@ Ext.define('moneyworld.controller.SummaryViewLifeExpectancy', {
 		},
 		control: {
 			'summaryViewLifeExpectancy': {
-				initialize: 'initHack'
-				// painted: 'myDrawFunctionHack'
+				initialize: 'initHack',
 			}
 		}
 	},
-	testFn: function() {
-		alert("success")
-	},
 	myCanvasObj: null,
-	counter: 0,
+	currentLifeExpectancy: null,
 	renderView: function() {
+		var settingsStore = Ext.getStore('Settings');
+		var dataSetsStore = Ext.getStore('DataSets');
+		var dataPointsStore;
+
+		var currentCountry;
+		var currentRegion;
+
+		var preHtmlString = this.preHtmlString;
+		var postHtmlString = this.postHtmlString;
+
+		var settingsRecords = [];
+		var dataPointsRecords = [];
+
+		settingsStore.load({
+			callback: loadDataSetsStore,
+			scope: this
+		});
+
+		function loadDataSetsStore(records, operations, success) {
+			settingsRecords = records;
+			currentCountry = records[0].get('countryCode');
+			postHtmlString += records[0].get('countryName');
+			currentRegion = records[0].get('region');
+			dataSetsStore.load({
+				callback: loadDataPointsStore,
+				scope: this
+			});
+		}
+
+		function loadDataPointsStore(records, operation, success) {
+			dataSetsRecords = records;
+			dataPointsStore = moneyworld.utils.Functions.getServerStore(
+				this.getSummaryViewLifeExpectancy().getDataSet(),
+				"all",
+				currentCountry);
+			dataPointsStore.load({
+				callback: setData,
+				scope: this
+			});
+		}
+
+		function setData(records, operation, success) {
+
+			// Visualisation code starts here
+			dataPointsStore.sort([{
+				property: 'year',
+				direction: 'ASC'
+			}]);
+			var dataSetID = this.getSummaryViewLifeExpectancy().getDataSet();
+			dataPointsStore.filter([
+				Ext.create('Ext.util.Filter', {
+					property: 'countryCode',
+					value: currentCountry
+				}),
+				Ext.create('Ext.util.Filter', {
+					property: 'dataSetCode',
+					value: dataSetID
+				})
+			]);
+			var currentLifeExpectancy = dataPointsStore.last().get('value');
+			this.currentLifeExpectancy = moneyworld.utils.Functions.prettify(currentLifeExpectancy, 1,"","");
+			var dataStoreRecord = dataSetsStore.findRecord('id', dataSetID);
+			if (this.myCanvasObj != undefined){
+				var canvas = this.myCanvasObj['canvases'][0]['dom'];
+				this.myDrawFunctionHack(canvas);
+			}
+		}
 	},
-	// init: function() {
-	// 	containerObj = Ext.get("summaryview_lifeExpectancy");
-	// 	if (containerObj !== null) {
-	// 		containerH = containerObj.getHeight();
-	// 		containerW = containerObj.getWidth();
-	// 		//Ext.getBody().getViewSize().height
-	// 		console.log(containerW || containerH);
-	// 		if (containerW || containerH !== 0) {
-	// 			// we test if the height and width are not zero
-	// 			var canvasObj = new Ext.draw.engine.Canvas({
-	// 				region: [0, 0, containerW, containerH] // this is the whole region of the canvas.
-	// 				// region: [0, 0, parseInt(myViewObj.getWidth())/10*screen.availHeight, 1000]
-	// 			});
-	// 			this.getSummaryViewLifeExpectancy().add(canvasObj);
-	// 			this.myCanvasObj = canvasObj;
-	// 			this.myDrawFunctionHack(this.myCanvasObj);
-	// 		}
-	// 	}
-	// },
 	initHack: function() {
 		var containerObj = Ext.get("summaryview_lifeExpectancy");
-
 		var canvasObj = new Ext.draw.engine.Canvas({
 			id: 'myCanvasLifeExpectancy',
-			region: [0, 0, document.body.clientWidth, Math.min(document.body.clientWidth*1.3,document.body.clientHeight-76)] // this is the whole region of the canvas.
+			region: [0, 0, document.body.clientWidth, Math.min(document.body.clientWidth * 1.3, document.body.clientHeight - 76)] // this is the whole region of the canvas.
 			// region: [0, 0, parseInt(myViewObj.getWidth())/10*screen.availHeight, 1000]
 		});
 		this.getSummaryViewLifeExpectancy().add(canvasObj);
 		this.myCanvasObj = canvasObj;
-		var canvas = canvasObj['canvases'][0]['dom'];
-		this.myDrawFunctionHack(canvas);
-
+		this.renderView();
 	},
 	myDrawFunctionHack: function(canvas) {
 		canvas.height = 390;
 		canvas.width = 300;
-		
-		var lifeExpectancy = 70;
-		
+		canvas.top = 50;
+
+		var lifeExpectancy = this.currentLifeExpectancy;
 		var ctx = canvas.getContext("2d");
-		
 		var imageSource = "resources/images/cake.png";
-		
+
 		var cake = new Image();
 		cake.src = imageSource;
-		cake.onload = function () {
+		cake.onload = function() {
 			ctx.drawImage(cake, 0, 0, 300, cake.height / cake.width * 300);
-			
-			ctx.rotate(18*Math.PI/180);
-
-			
-			ctx.font = "bold 45px sans-serif";
+			ctx.rotate(18 * Math.PI / 180);
+			ctx.font = "bold 40px sans-serif";
 			ctx.fillStyle = "#666";
 			ctx.fillText(lifeExpectancy + " years", 110, 95);
-			
+
 		};
 
 	}
