98d67838d79e3810ac4edb5f22f40ce3270b8e22
Darren Foong <darrenfoong@gmail.com>
2014-02-25 14:44:08 +0000

    Added InfoView and changed navigation bar behaviour

---
+++ client/app/controller/InfoView.js
@@ -0,0 +1,31 @@
+Ext.define('moneyworld.controller.InfoView', {
+	extend: 'Ext.app.Controller',
+
+	config: {
+		refs: {
+			mainView: 'main',
+			infoView: 'infoview'
+		},
+		control: {
+			'infoView': {
+				initialize: 'renderView'
+			}
+		}
+	},
+
+	renderView: function() {
+		console.log("Loading InfoView");
+
+		var dataSetsStore = Ext.getStore('DataSets');
+		dataSetsStore.load({ callback: loadInfo, scope: this });
+
+		function loadInfo(records, operation, success) {
+			var currentDataSet = dataSetsStore.findRecord('id', this.getInfoView().getDataSet());
+			var currentDataSetName = currentDataSet.get('name');
+			var currentDataSetDescription = currentDataSet.get('description');
+
+			moneyworld.utils.Functions.changeTitle(this.getInfoView(), this.getMainView(), currentDataSetName);
+			this.getInfoView().setHtml("<h1>" + currentDataSetName + "</h1><p>" + currentDataSetDescription + "</p>");
+		}
+	}
+});
---
+++ client/app/view/InfoView.js
@@ -0,0 +1,16 @@
+Ext.define('moneyworld.view.InfoView', {
+	extend: 'Ext.Panel',
+	xtype: 'infoview',
+	requires: [
+	],
+	config: {
+		title: null,
+
+		dataSet: null,
+		country: null,
+
+		styleHtmlContent: true,
+		fullscreen: true,
+		layout: 'vbox'
+	}
+});
