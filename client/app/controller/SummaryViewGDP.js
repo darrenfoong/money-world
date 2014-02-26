ce8fc399e34308095ab02cf3520e2cc3e003f2cc
Daniel Low <daniellowtw@gmail.com>
2014-02-26 00:54:10 +0000

    fixed circle on phone

--- client/app/controller/SummaryViewGDP.js
+++ client/app/controller/SummaryViewGDP.js
@@ -4,17 +4,17 @@ Ext.define('moneyworld.controller.SummaryViewGdp', {
 	config: {
 		refs: {
 			mainView: 'main',
-			summaryViewGDP: 'summaryview_gdp'
+			summaryViewGdp: 'summaryview_gdp'
 		},
 		control: {
-			'summaryViewGDP': {
+			'summaryViewGdp': {
 				initialize: 'renderView'
 			}
 		}
 	},
-	
+
 	renderView: function() {
-		
+
 		var settingsStore = Ext.getStore('Settings');
 		var dataSetsStore = Ext.getStore('DataSets');
 		var dataPointsStore;
@@ -24,17 +24,23 @@ Ext.define('moneyworld.controller.SummaryViewGdp', {
 
 		var settingsRecords = [];
 		var dataPointsRecords = [];
-		
+
 		var maxGDP = 0;
 		var currentGDP;
-		
-		settingsStore.load({ callback: loadDataSetsStore, scope: this });
-		
+
+		settingsStore.load({
+			callback: loadDataSetsStore,
+			scope: this
+		});
+
 		function loadDataSetsStore(records, operations, success) {
 			settingsRecords = records;
 			currentCountry = records[0].get('countryCode');
 			currentRegion = records[0].get('region');
-			dataSetsStore.load({ callback: loadDataPointsStore, scope: this });
+			dataSetsStore.load({
+				callback: loadDataPointsStore,
+				scope: this
+			});
 		}
 
 		function loadDataPointsStore(records, operation, success) {
@@ -43,66 +49,82 @@ Ext.define('moneyworld.controller.SummaryViewGdp', {
 				this.getSummaryViewGdp().getDataSet(),
 				"all",
 				currentCountry);
-			dataPointsStore.load({ callback: setData, scope: this });
+			dataPointsStore.load({
+				callback: setData,
+				scope: this
+			});
 		}
-		
+
 		function setData(records, operation, success) {
 			// Visualisation code starts here
-			dataPointsStore.sort([{ property: 'year', direction: 'ASC'}]);
+			dataPointsStore.sort([{
+				property: 'year',
+				direction: 'ASC'
+			}]);
 			var dataSetID = this.getSummaryViewGdp().getDataSet();
-			dataPointsStore.filter([Ext.create('Ext.util.Filter', { filterFn: function(dataPoint) {
+			dataPointsStore.filter([Ext.create('Ext.util.Filter', {
+				filterFn: function(dataPoint) {
 					var currentValue = parseInt(dataPoint.get('value'));
 					if (currentValue > maxGDP) maxGDP = currentValue;
 					return true;
-				}})
-			]);
+				}
+			})]);
 			dataPointsStore.filter([
-				Ext.create('Ext.util.Filter', { property: 'countryCode', value: currentCountry }),
-				Ext.create('Ext.util.Filter', { property: 'dataSetCode', value: this.getSummaryViewGdp().getDataSet() })
+				Ext.create('Ext.util.Filter', {
+					property: 'countryCode',
+					value: currentCountry
+				}),
+				Ext.create('Ext.util.Filter', {
+					property: 'dataSetCode',
+					value: this.getSummaryViewGdp().getDataSet()
+				})
 			]);
 			currentGDP = dataPointsStore.last().get('value');
 			console.log(currentGDP);
 			console.log(maxGDP);
-			console.log(currentGDP/maxGDP);
-		
+			console.log(currentGDP / maxGDP);
+
 			var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-		
+
 			var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-		
-			height = height*0.9;
-		
-			var centrex = width/2, centrey = (height-46-28)/2, circler = ((centrex < centrey) ? centrex : centrey) * (currentGDP/maxGDP) * 0.9;
-		
-			var colourString = '#79BB3F';			
-			
-			this.getSummaryViewGdp().add({
+
+			height = height * 0.9;
+
+			var centrex = width / 2,
+				centrey = (height - 46 - 28) / 2,
+				circler = ((centrex < centrey) ? centrex : centrey) * (currentGDP / maxGDP) * 0.9;
+
+			var colourString = '#79BB3F';
+
+			var circlePanel = Ext.create('Ext.draw.Component', {
 				xtype: 'panel',
 				layout: 'card',
 				height: '90%',
 				width: '100%',
-				style: {
-					background: 'red'
-				},
 				initialize: function() {
-					var drawComponent1 = Ext.create('Ext.draw.Component', {});
-					drawComponent1.getSurface('main').add({
-						type: 'circle',
-						fill: colourString,
-						radius: circler,
-						x: centrex,
-						y: centrey
-					});
-					this.add(drawComponent1);
+					console.log("ok");
 				}
 			});
-		
+
+			var drawComponent1 = Ext.create('Ext.draw.Component', {});
+			drawComponent1.getSurface('main').add({
+				type: 'circle',
+				fill: colourString,
+				radius: circler,
+				x: centrex,
+				y: centrey
+			});
+			// console.log(this);
+			circlePanel.add(drawComponent1);
+			this.getSummaryViewGdp().add(circlePanel);
+
 			this.getSummaryViewGdp().add({
 				xtype: 'panel',
 				layout: 'card',
 				height: '10%',
 				width: '100%',
 				style: 'text-align: center',
-				html: "<div>GDP per capita is $" + parseFloat(currentGDP).toFixed(2) +"</div>"
+				html: "<div>GDP per capita is $" + parseFloat(currentGDP).toFixed(2) + "</div>"
 			});
 		}
 	}
