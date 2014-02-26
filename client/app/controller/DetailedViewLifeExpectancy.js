21811accf0fefb89dce32bd894a66d3bd3714a7a
Darren Foong <darrenfoong@gmail.com>
2014-02-26 09:35:40 +0000

    Changed colours of graphs in DetailedViews

--- client/app/controller/DetailedViewLifeExpectancy.js
+++ client/app/controller/DetailedViewLifeExpectancy.js
@@ -55,7 +55,7 @@ Ext.define('moneyworld.controller.DetailedViewLifeExpectancy', {
 							xField: 'year',
 							yField: 'value',
 							style: {
-								stroke: '#115fa6',
+								stroke: '#dddddd',
 								lineWidth: 3,
 								shadowColor: 'rgba(0,0,0,0.7)',
 								shadowBlur: 10,
@@ -64,8 +64,8 @@ Ext.define('moneyworld.controller.DetailedViewLifeExpectancy', {
 							},
 							marker: {
 								type: 'circle',
-								stroke: '#0d1f96',
-								fill: '#115fa6',
+								stroke: '#000000',
+								fill: '#777777',
 								lineWidth: 2,
 								radius: 4,
 								shadowColor: 'rgba(0,0,0,0.7)',
