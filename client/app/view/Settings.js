ddd6a02ee86438d114c209ffe6ee3329419be7b6
Darren Foong <darrenfoong@gmail.com>
2014-02-28 21:09:49 +0000

    Removed name field from Settings

--- client/app/view/Settings.js
+++ client/app/view/Settings.js
@@ -8,16 +8,16 @@ Ext.define('moneyworld.view.Settings', {
 	config: {
 		title: 'Settings',
 
+		layout: {
+			type: 'vbox',
+			pack: 'center'
+		},
+
 		items: [
 			{
 				xtype: 'fieldset',
 
 				items: [
-					{
-						name: 'username',
-						xtype: 'textfield',
-						label: 'Name'
-					},
 					{
 						name: 'countryCode',
 						xtype: 'selectfield',
@@ -32,7 +32,7 @@ Ext.define('moneyworld.view.Settings', {
 					}
 				],
 
-				instructions: "Please choose your home country. <br/> 1. Swipe to see different indicators, and tap for more detail. <br/> 2. Tap on the globe icon to see the indicator on a map. <br/> 3. Tap the i button for an explanation of the indicator."
+				instructions: "Please choose your home country<p>1. Swipe to see different indicators, and tap for more detail.</p><p>2. Tap on the globe icon to see the indicator on a map.<p>3. Tap the i button for an explanation of the indicator.</p>"
 			}
 		]
 	}
